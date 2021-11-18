e71a75e285d40dd1fd01271e8f66ef514a629617
LucasMastelini <lucas.vmelo@bandtec.com.br>
2021-11-18 19:22:11 -0300

    implementações e mudanças

--- scrip cadastro.sql
+++ scrip cadastro.sql
@@ -2,19 +2,31 @@ create database FPS;
 
 use FPS;
 
-create table cadastro(
-idcadastro int ,
-primary key (nickName, Email),
+create table usuario(
+idusuario int primary key auto_increment,
 nome varchar(45),
-sobrenome varchar(45),
 nickName varchar(45),
 email varchar(100),
-senha varchar(20),
-confirmarSenha varchar(20));
+senha varchar(100)
+);
 
 
-create table login(
-idlogin int primary key auto_increment,
-nickName varchar(45),
-email varchar(100),
-senha varchar(20));
+create table jogos (
+idjogo int primary key auto_increment,
+fkJogador int,
+foreign key (fkjogador) references jogador (idjogador)
+);
+ 
+ create table jogador(
+ idjogador int primary key auto_increment,
+ nome varchar(45),
+ jogo varchar(45)
+ );
+
+create table ranking(
+fkjogos int,
+foreign key (fkjogos) references usuario (idjogos),
+fkusuario int,
+foreign key (fkusuario) references usuario (idusuario)
+);
+
